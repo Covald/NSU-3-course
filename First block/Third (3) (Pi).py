8bd68a0fd2fa8fb6590cc1e557bd10173d8704e4
Covald <rezin.p@gmail.com>
2020-09-17 19:33:04 +0700

    Solve third task, but need a comment by teacher

---
+++ First block/Third (3) (Pi).py
@@ -0,0 +1,5 @@
+import math
+# Machin's formula
+print(4 * (4* math.atan(1/5)-math.atan(1/239)))
+print(round(4 * (4* math.atan(1/5)-math.atan(1/239)),int(input("До какого знака считать? \nОтвет - "))))
+print(math.pi)
