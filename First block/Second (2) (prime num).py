e1e706fefc99d859190583b6b74bbf0d3b9caba1
Covald <rezin.p@gmail.com>
2020-09-18 18:21:46 +0700

    Correct second task

--- First block/Second (2) (prime num).py
+++ First block/Second (2) (prime num).py
@@ -1,4 +1,3 @@
 import math
-
-print([2] + [x for x in range(3, int(input("Input N - ")), 2)
-             if all(x % i != 0 for i in range(3, int(math.sqrt(x)) + 1, 2))])
+print([2] + [x for x in range(3, int(input("Input N - "))+1, 2)
+             if all(x % i for i in range(3, int(math.sqrt(x)) + 1, 2))])
