f454024b29689f5fae09e61ba5c89f70f1e04153
Covald <rezin.p@gmail.com>
2020-09-21 13:12:00 +0700

    Correct second task

--- First block/First (1) (Квадратное уравнение).py
+++ First block/First (1) (Квадратное уравнение).py
@@ -4,10 +4,14 @@ import math
 def func(a, b, c):
     d = b ** 2 - 4 * a * c
     print(f"Discriminant is {d.real}")
-    if d==0:
-        return [-b / (2 * a)]
+    if a.real==0 and b.real==0 :
+        return [None]
+    elif a.real==0:
+        return [-c/b]
+    elif d==0:
+        return [-b.real / (2 * a.real)]
     else:
-        return [(-b + pow(d.real, 0.5)) / (2 * a), (-b - pow(d.real, 0.5)) / (2 * a)]
+        return [(-b.real + pow(d.real, 0.5)) / (2 * a.real), (-b.real - pow(d.real, 0.5)) / (2 * a.real)]
 
 
 a, b, c = map(complex, input("Input a,b,c from ax^2+bx+c=0 - ").split())
--- First block/Second (2) (prime num).py
+++ First block/Second (2) (prime num).py
@@ -1,3 +1,7 @@
 import math
 print([2] + [x for x in range(3, int(input("Input N - "))+1, 2)
              if all(x % i for i in range(3, int(math.sqrt(x)) + 1, 2))])
+
+
+x = 3000
+print(all(x % i for i in range(3, int(math.sqrt(x)) + 1, 2)))
