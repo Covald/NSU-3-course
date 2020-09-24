85ef0cea19ff950bffb633cfea64d2c444923790
Covald <rezin.p@gmail.com>
2020-09-24 13:52:47 +0700

    Correct second task

--- First block/Second (2) (prime num).py
+++ First block/Second (2) (prime num).py
@@ -1,7 +1,8 @@
 import math
-print([2] + [x for x in range(3, int(input("Input N - "))+1, 2)
-             if all(x % i for i in range(3, int(math.sqrt(x)) + 1, 2))])
 
 
-x = 3000
-print(all(x % i for i in range(3, int(math.sqrt(x)) + 1, 2)))
+def prime_numbers(N):
+    print([2] + [x for x in range(3, N + 1, 2) if all(x % i for i in range(3, int(math.sqrt(x)) + 1, 2))])
+
+
+prime_numbers(int(input("Input N - ")))
--- First block/Third (3) (Pi).py
+++ First block/Third (3) (Pi).py
@@ -1,5 +1,19 @@
 import math
-# Machin's formula
-print(4 * (4* math.atan(1/5)-math.atan(1/239)))
-print(round(4 * (4* math.atan(1/5)-math.atan(1/239)),int(input("До какого знака считать? \nОтвет - "))))
-print(math.pi)
+from decimal import *
+
+N = 20
+K = N+500
+
+print(Decimal(math.pi).quantize(Decimal("1."+"0"*N)))
+getcontext().prec = 50
+
+
+def elem_chud(y):
+    return Decimal(Decimal(Decimal(pow(-1, y)) * Decimal(math.factorial(6 * y)) * Decimal(13591409 + 545140134 * y)) /
+                   Decimal(Decimal((math.factorial(3 * y)) * (Decimal(math.factorial(y))**Decimal("3")) *
+                           Decimal(Decimal("640320") ** Decimal(3 * y + Decimal("1.5"))))))
+
+print([elem_chud(i) for i in range(K + 1)])
+pi = (Decimal("12") * Decimal(sum([elem_chud(i) for i in range(K + 1)]))) ** -1
+
+print(pi.quantize(Decimal("1." + "0" * N)))
