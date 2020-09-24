e01c0784c3d445a0f31b2538c7bea3f1560cc410
Covald <rezin.p@gmail.com>
2020-09-24 18:42:35 +0700

    Correct second task and solve the forth task

--- First block/Third (3) (Pi).py
+++ First block/Third (3) (Pi).py
@@ -2,18 +2,27 @@ import math
 from decimal import *
 
 N = 20
-K = N+500
+K = N + 500
 
-print(Decimal(math.pi).quantize(Decimal("1."+"0"*N)))
+print(Decimal(math.pi).quantize(Decimal("1." + "0" * N)))
 getcontext().prec = 50
 
 
 def elem_chud(y):
-    return Decimal(Decimal(Decimal(pow(-1, y)) * Decimal(math.factorial(6 * y)) * Decimal(13591409 + 545140134 * y)) /
-                   Decimal(Decimal((math.factorial(3 * y)) * (Decimal(math.factorial(y))**Decimal("3")) *
-                           Decimal(Decimal("640320") ** Decimal(3 * y + Decimal("1.5"))))))
+    return Decimal(Decimal(Decimal(Decimal((-1)**y) *
+                           Decimal(math.factorial(6 * y)) * (
+                           Decimal("13591409") + Decimal("545140134") * y))) /
+                   Decimal(Decimal((math.factorial(3 * y)) *
+                                   (Decimal(math.factorial(y)) ** Decimal("3")) *
+                                   Decimal(Decimal("640320") ** (Decimal(3 * y) + Decimal("1.5"))))))
 
-print([elem_chud(i) for i in range(K + 1)])
-pi = (Decimal("12") * Decimal(sum([elem_chud(i) for i in range(K + 1)]))) ** -1
+
+# print([elem_chud(i) for i in range(K + 1)])
+pi = (Decimal("12") * Decimal(sum([elem_chud(Decimal(i)) for i in range(K + 1)]))) ** -1
 
 print(pi.quantize(Decimal("1." + "0" * N)))
+
+"""
+Сделать невозможно в условиях краткости кода (нужно писать свои типы данных из разряда - вещественное как список.
+Проблема в потере знака после 16го разряда
+"""
