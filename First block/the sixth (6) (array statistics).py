9d4b4a8bfd3571607e7e80100753ec9a2de2fa77
Covald <rezin.p@gmail.com>
2020-09-26 19:00:37 +0700

    Solve sixth task

--- First block/the sixth (6) (array statistics).py
+++ First block/the sixth (6) (array statistics).py
@@ -1,10 +1,11 @@
 import numpy as np
 from scipy import stats
 
-array = np.random.randint(0, 100, 10)
+array = np.random.randint(0, 101, 1000)
 
 print(array)
 print(f"min - {min(array)}, max - {max(array)} ")
 print(f"average - {np.average(array)}")
 print(f"std - {np.std(array)}")
-print(f"xui - {stats.moment(array, 5)}")
+print(f"Fifth central moment - {stats.moment(array, 5)}")
+print(f"Среднее вышеуказанных чисел - {np.average(np.array([np.min(array), np.max(array), np.average(array), np.std(array), stats.moment(array, 5)]))}")
