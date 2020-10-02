f1ebdc4628a3e53454d7c32db348a29897ef79f0
Covald <rezin.p@gmail.com>
2020-10-02 21:14:06 +0700

    Solve 14

--- First block/(14).py
+++ First block/(14).py
@@ -16,19 +16,21 @@ def fill_dyn_matrix(x, y):
                 L[x_i][y_i] = max((L[x_i][y_i - 1], L[x_i - 1][y_i]))
     return L
 
+
 def LCS_DYN(x, y):
     L = fill_dyn_matrix(x, y)
     LCS = []
-    x_i,y_i = len(x)-1,len(y)-1
+    x_i, y_i = len(x) - 1, len(y) - 1
     while x_i >= 0 and y_i >= 0:
         if x[x_i] == y[y_i]:
             LCS.append(x[x_i])
-            x_i, y_i = x_i-1, y_i-1
-        elif L[x_i-1][y_i] > L[x_i][y_i-1]:
+            x_i, y_i = x_i - 1, y_i - 1
+        elif L[x_i - 1][y_i] > L[x_i][y_i - 1]:
             x_i -= 1
         else:
             y_i -= 1
     LCS.reverse()
     return LCS
+
 
 print(LCS_DYN(x, y))
