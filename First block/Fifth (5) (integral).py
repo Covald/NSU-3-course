4e08585f963795457d69d4b991249b26b0f00e3e
Covald <rezin.p@gmail.com>
2020-10-14 19:58:45 +0700

    correct fifth task

--- First block/Fifth (5) (integral).py
+++ First block/Fifth (5) (integral).py
@@ -1,19 +1,24 @@
 from sympy import symbols, integrate
 
-"""
-x/((x**2+1)**3)
-sqrt(2) oo
-"""
 
-x = symbols("x")
+def main(expr=None, a=None, b=None):
+    """
+    x/((x**2+1)**3)
+    sqrt(2) oo
+    """
+
+    x = symbols("x")
+    if not expr:
+        expr = input("Enter expression - ")
+    if not a and not b:
+        a, b = input("Enter a, b - ").split()
+    print(expr + "\n" + f"a = {a} \nb = {b}")
+    print(integrate(expr, x).n())
+    print(integrate(expr, (x, a, b)))
 
 
-expr = input("Enter expression - ")
+def test():
+    main("x/((x**2+1)**3)", "sqrt(2)", "oo")
 
-a, b = input("Enter a, b - ").split()
 
-Integ = integrate(expr, x)
-Integ1 = integrate(expr, (x, a, b))
-
-print(Integ)
-print(Integ1.n())
+test()
