f01f4a5c21b7ae2f47a36ab6725df3651fd68e7b
Covald <rezin.p@gmail.com>
2020-10-14 19:50:53 +0700

    correct first task

--- First block/First (1) (Квадратное уравнение).py
+++ First block/First (1) (Квадратное уравнение).py
@@ -1,14 +1,11 @@
-import math
-
-
 def func(a, b, c):
     d = b ** 2 - 4 * a * c
     print(f"Discriminant is {d.real}")
-    if a.real==0 and b.real==0 :
+    if a.real == 0 and b.real == 0:
         return [None]
-    elif a.real==0:
-        return [-c/b]
-    elif d==0:
+    elif a.real == 0:
+        return [-c / b]
+    elif d == 0:
         return [-b.real / (2 * a.real)]
     else:
         return [(-b.real + pow(d.real, 0.5)) / (2 * a.real), (-b.real - pow(d.real, 0.5)) / (2 * a.real)]
