4073a8ece672127f241a767fd4279832408ac70e
Covald <rezin.p@gmail.com>
2020-11-03 19:01:52 +0700

    new venv (python 3.9) correct 10 update gitignore

--- First block/Matrix/Matrix (10).py
+++ First block/Matrix/Matrix (10).py
@@ -2,57 +2,59 @@ import numpy as np
 import timeit
 from time import time
 
+a = int(input('Введите число строк: '))
+b = int(input('Введите число столбцов: '))
+m1 = np.random.randint(-50, 50, (a, b))
+m2 = np.random.randint(-50, 50, (a, b))
+print(m1, m2, sep='\n\n', end='\n\n\n')
 
-def main():
-    a = int(input('Введите число строк: '))
-    b = int(input('Введите число столбцов: '))
-    m1 = np.random.randint(-50, 50, (a, b))
-    m2 = np.random.randint(-50, 50, (a, b))
-    print(m1, m2, sep='\n\n', end='\n\n\n')
+print('Сумма: \n', m1 + m2, '\n\nПроизведение: \n', m1 * m2)
 
-    print('Сумма: \n', m1 + m2, '\n\nПроизведение: \n', m1 * m2)
+try:
+    print('\n\nОпределитель первой матрицы: ', np.linalg.det(m1))
+    print('Определитель второй матрицы: ', np.linalg.det(m2))
+except:
+    print('\n\nНевозможно вычислить определители')
+
+try:
+    print('\n\nОбратная матрица для первой:\n', np.linalg.inv(m1))
+    print('\n\nОбратная матрица для второй:\n', np.linalg.inv(m2))
+except:
+    print('\n\nОбратные матрицы не существуют')
+
+
+def save_matrix(matrix):
+    try:
+        with open('result.txt', 'w') as g:
+            g.write(str(matrix))
+
+    except Exception as error:
+        print('\n', error)
 
     try:
-        print('\n\nОпределитель первой матрицы: ', np.linalg.det(m1))
-        print('Определитель второй матрицы: ', np.linalg.det(m2))
-    except:
-        print('\n\nНевозможно вычислить определители')
-
-    try:
-        print('\n\nОбратная матрица для первой:\n', np.linalg.inv(m1))
-        print('\n\nОбратная матрица для второй:\n', np.linalg.inv(m2))
-    except:
-        print('\n\nОбратные матрицы не существуют')
-
-    def save_matrix(matrix, filename: str = "matrix.txt"):
-        try:
-            np.savetxt(filename, matrix, delimiter=",")
-        except Exception as err:
-            print(err)
-
-    def load(filename: str = "matrix.txt") -> np.array:
-        try:
-            matrix = np.loadtxt(filename, delimiter=",")
-            print('\nОбратная матрица из файла:\n', np.linalg.inv(matrix))
-            return matrix
-        except Exception as err:
-            print(err)
-
-    start = time()
-    np.linalg.inv(np.random.randint(-50, 50, (10, 10)))
-    print('\nВремя обращения матрицы 10х10: ', time() - start)
-
-    start = time()
-    np.linalg.inv(np.random.randint(-50, 50, (100, 100)))
-    print('\nВремя обращения матрицы 100х100: ', time() - start)
-
-    start = time()
-    np.linalg.inv(np.random.randint(-50, 50, (1000, 1000)))
-    print('\nВремя обращения матрицы 1000х1000: ', time() - start)
-
-    save_matrix(m1)
-    m1 = load()
+        with open('matrix') as f:
+            l = f.readlines()
+            l1 = [list(map(int, l[i].split())) for i in range(len(l))]
+            m = np.array(l1)
+            print('\nОбратная матрица из файла:\n', np.linalg.inv(m))
+    except Exception as err:
+        print(err)
 
 
-if __name__ == "__main__":
-    main()
+start = time()
+np.linalg.inv(np.random.randint(-50, 50, (10, 10)))
+print('\nВремя обращения матрицы 10х10: ', time() - start)
+
+start = time()
+np.linalg.inv(np.random.randint(-50, 50, (100, 100)))
+print('\nВремя обращения матрицы 100х100: ', time() - start)
+
+start = time()
+np.linalg.inv(np.random.randint(-50, 50, (1000, 1000)))
+print('\nВремя обращения матрицы 1000х1000: ', time() - start)
+
+# start = time()
+# np.linalg.inv(np.random.randint(-50, 50, (8728, 8728)))
+# print('\nВремя обращения матрицы 8728х8728(макс): ', time() - start)
+
+save_matrix(np.linalg.inv(m1))
