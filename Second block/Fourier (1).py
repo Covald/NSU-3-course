e1f1d6eb3bb849bb8fb1c038e6774de8c5760e20
pv.rezin <rezin.p@gmail.com>
2020-12-10 20:09:37 +0700

    Change fourier

--- Second block/Fourier (1).py
+++ Second block/Fourier (1).py
@@ -1,84 +1,85 @@
-import sys
-import time
-
 from numpy.random import uniform
-from numpy import pi, sin, array, linspace, arange
+from numpy import pi, sin, array, linspace, arange, abs as np_abs
 import numpy as np
 from pylab import subplot, plot, xlabel, ylabel, show, axis, axes
-from numpy.fft import rfft, irfft
-import scipy as sp
+from numpy.fft import rfft, irfft, rfftfreq
 
-FD = 40800  # discr frequency
-t = 20    # 20 sec 20/FD 0.0001
-t = linspace(0, t*2*pi, int(2*pi*FD))
+FD = 1024  # discr frequency 1/Td
+time = 200  # sec
+BORDER = 0.15  # граница среза от макс аплитуды
+
+f_first = 4.8
+f_second = 5
+f_third = 5.2
+
+A_first = 5
+A_second = 7
+A_third = 9
+
+w_first = (2. * pi * f_first / FD)
+w_second = (2. * pi * f_second / FD)
+w_third = (2. * pi * f_third / FD)
+
+N = time * FD  # len of array
+t = arange(N)
+
+sig_first = A_first * sin(w_first * t)
+sig_second = A_second * sin(w_second * t)
+sig_third = A_third * sin(w_third * t)
 
 
-def plot_spectrum(signal: array, pos: list or tuple):
-    n = len(signal)  # length of the signal
+def plot_spectrum(signal: array, pos: int):
+    frq = rfftfreq(N, 1. / FD)
 
-    k = arange(n)
+    spectrum = rfft(signal) / N  # fft computing and normalization
 
-    T = n / FD
-
-    frq = k / T  # two sides frequency range
-    frq = frq[range(int(n / 2))]  # one side frequency range
-
-    y = rfft(signal) / n  # fft computing and normalization
-    y = y[range(int(n / 2))]
-
-    subplot(*pos).grid(True)  # plotting the spectrum
-    plot(frq, abs(y), 'r')
+    subplot(pos).grid(True)  # plotting the spectrum
+    plot(frq, np_abs(spectrum), 'r')
     xlabel('Freq (Hz)')
     ylabel('|Y(freq)|')
 
 
 def main():
-    first = 7 * sin(2 * pi * 4.8 * t)
-    second = 7 * sin(2 * pi * 5 * t)
-    third = 7 * sin(2 * pi * 5.2 * t)
-    signal = first + second + third
+    signal = sig_first + sig_second + sig_third
     noise = (np.random.random(t.shape) - 0.5) * np.random.random(t.shape) * 50
 
-    subplot(2, 3, 1).grid(True)
-    plot(t, third, "green")
-    plot(t, second, "yellow")
-    plot(t, first, "blue")
-    plot(t, signal, "red")
+    subplot(231).grid(True)
+    plot(t / FD, sig_third, "green")
+    plot(t / FD, sig_second, "yellow")
+    plot(t / FD, sig_first, "blue")
+    plot(t / FD, signal, "red")
     xlabel('Time')
     ylabel('Amplitude')
 
-    plot_spectrum(signal, [2, 3, 4])
+    plot_spectrum(signal, 234)
 
-    subplot(2, 3, 2).grid(True)
+    subplot(232).grid(True)
     signal_with_noise = signal + noise
-    plot(t, signal_with_noise, "b")
-    plot(t, signal, "r")
+    plot(t / FD, signal_with_noise, "b")
+    plot(t / FD, signal, "r")
     xlabel('Time')
     ylabel('Amplitude')
 
-    plot_spectrum(signal_with_noise, (2, 3, 5))
+    plot_spectrum(signal_with_noise, 235)
 
-    n = len(signal_with_noise)  # length of the signal
-    k = arange(n)
-    T = n / FD
-    frq = k / T  # two sides frequency range
-    frq = frq[range(int(n / 2))]  # one side frequency range
-    y = rfft(signal_with_noise) / n  # fft computing and normalization
-    y = y[range(int(n / 2))]
+    frq = rfftfreq(N, 1. / FD)
 
-    border = np.amax(np.absolute(y)) * 0.20  # Все сигналы меньше 20% максимальной амплитуды будем считать шумом
-    y_cut = y.copy()
-    y_cut[np.absolute(y) < border] = 0
+    spectrum = rfft(signal) / N  # fft computing and normalization
+
+    # Все сигналы меньше 20% максимальной амплитуды будем считать шумом
+    border = np.amax(np.absolute(spectrum)) * BORDER
+    spectrum_cut = spectrum.copy()
+    spectrum_cut[np.absolute(spectrum) < border] = 0
 
     subplot(236).grid(True)
-    plot(frq, abs(y_cut), 'r')
+    plot(frq, abs(spectrum_cut), 'r')
     xlabel('Freq (Hz)')
     ylabel('|Y(freq)|')
 
-    signal = irfft(y_cut) * n
+    signal = irfft(spectrum_cut) * N
 
     subplot(233).grid(True)
-    plot(t[:-3], signal, "red")
+    plot(t / FD, signal, "red")
     xlabel('Time')
     ylabel('Amplitude')
 
