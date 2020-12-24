b8bd65b82c3f273b0b12516960cd03f5b675d37b
pv.rezin <rezin.p@gmail.com>
2020-12-24 15:25:59 +0700

    Доделал 2ю из третьего блока

--- Third block/Напоминалка/deadinside.py
+++ Third block/Напоминалка/deadinside.py
@@ -1,9 +1,18 @@
+from __future__ import print_function
 import time
 from datetime import datetime
 import pickle
+import smtplib
+import os
+import envi
 
-print(time.time())
-_TIMER = 1
+HOST = "smtp.gmail.com"
+PORT = 465
+TO = "rezin.p@gmail.com"
+FROM = "p.rezin@g.nsu.ru"
+
+LOGIN = os.environ.get("LOGIN")
+PASSWORD = os.environ.get("PASSWORD")
 
 
 def _parse(line: list) -> dict:
@@ -15,23 +24,76 @@ def _parse(line: list) -> dict:
 def _load_from_file(file="Names and dates.txt"):
     test = {}
     with open(file, "r") as f:
-        print(list(f))
         for line in f:
+            print(line)
             l = line.split()
             test.update(_parse(l))
     return test
 
 
-def send_email(item: dict):
-    print(item)
+def create_message(sender, to, subject, message_text):
+    sent_from = sender
+    to = [to]
+    subject = subject
+    body = message_text
+
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+    
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+    return email_text
+
+
+def send_email(item):
+    print('asd') # abxnntlgzzkccdpe
+    LOGIN = os.environ.get("LOGIN")
+    PASSWORD = os.environ.get("PASSWORD")
+
+    sent_from = LOGIN
+    to = ['me@gmail.com', 'bill@gmail.com']
+    subject = 'OMG Super Important Message'
+    body = item
+
+    email_text = """\
+    From: %s
+    To: %s
+    Subject: %s
+
+    %s
+    """ % (sent_from, ", ".join(to), subject, body)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        #server.starttls()
+        server.login(LOGIN, PASSWORD)
+        server.sendmail(LOGIN, TO, email_text)
+    except Exception as err:
+        print(f"\n Error - {err} \n")
+
 
 
 def main():
+    envi.setenv()
     dict_of_file = _load_from_file()
+    print(dict_of_file)
     while True:
         now = datetime.now()
-        if now  in map(pickle.loads, dict_of_file.keys()):
-            send_email(dict_of_file.get(pickle.dumps(now+_TIMER)))
+        print(f"now - {now}")
+        send = []
+        for date in list(map(pickle.loads, dict_of_file.keys())):
+            print(date)
+            print(f"{date.timestamp()} + {60 * 60} = {date.timestamp() + 60 * 60} --- {now.timestamp()}")
+            if date.timestamp() + 60 * 60 <= now.timestamp():
+                print(f"send date - {date}")
+                send.append(dict_of_file.get(pickle.dumps(date)))
+
+        for i in send:
+            send_email(i)
+        time.sleep(1)
 
 
 if __name__ == '__main__':
